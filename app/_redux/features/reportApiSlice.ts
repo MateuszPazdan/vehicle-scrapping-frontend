fabf0651002b1ab630a2a60f0dbbc0ec9305b327
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2025-05-21 17:54:40 +0200

    change name of generated storage status report

--- app/_redux/features/reportApiSlice.ts
+++ app/_redux/features/reportApiSlice.ts
@@ -10,11 +10,16 @@ const reportApiSlice = apiSlice.injectEndpoints({
 					if (response.status !== 200) {
 						throw new Error('Wystąpił błąd przy generowaniu raportu.');
 					}
+					const formattedDate = new Date().toLocaleDateString('pl-PL');
+					const formattedTime = new Date().toLocaleTimeString('pl-PL', {
+						hour: '2-digit',
+						minute: '2-digit',
+					});
 					const blob = await response.blob();
 					const url = window.URL.createObjectURL(blob);
 					const link = document.createElement('a');
 					link.href = url;
-					link.download = `test.pdf`;
+					link.download = `Stan magazynowy ${formattedDate}, ${formattedTime}.pdf`;
 					document.body.appendChild(link);
 					link.click();
 					link.remove();
